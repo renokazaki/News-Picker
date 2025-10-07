f9c5021b834389e41be63dc04aa5c343a78578a4
ren <ren84690@gmail.com>
2025-10-08 08:30:30 +0900

    feature/update-delete-feature

--- src/app/(pages)/dashboard/components/dashboard/SideBar/interest/InterestList.tsx
+++ src/app/(pages)/dashboard/components/dashboard/SideBar/interest/InterestList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { getInterest } from "./getInterest";
+import { getInterest } from "./handleInterest";
+import { EditInterest } from "./EditInterest";
+import DeleteInterest from "./DeleteInterest";
 
 const InterestList = async ({ user_clerk_id }: { user_clerk_id: string }) => {
   const interests = await getInterest(user_clerk_id);
@@ -7,8 +9,15 @@ const InterestList = async ({ user_clerk_id }: { user_clerk_id: string }) => {
   return (
     <div className="w-full flex flex-col mt-4 space-y-2">
       {interests?.map((interest) => (
-        <div key={interest.id} className="w-full border border-gray-300 p-2">
+        <div
+          key={interest.id}
+          className="w-full border flex justify-between items-center border-gray-300 p-2"
+        >
           {interest.interest}
+          <div className="flex gap-2">
+            <DeleteInterest interestId={interest.id} />
+            <EditInterest interestId={interest.id} />
+          </div>
         </div>
       ))}
     </div>
